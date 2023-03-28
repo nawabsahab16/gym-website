2984ddb618a5c94f56b4e0cd7604c3fa7697bb77
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-28 21:15:00 +0530

    js basic structure

--- src/app.js
+++ src/app.js
@@ -4,14 +4,21 @@ import { Box } from '@mui/material';
 
 
 import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 
 const App = () => {
   return (
-    <Box width="400px">
-        Navbar 
-         <Route>
-            <Route path="/" element={<h1> Home</h1>} />
-         </Route>
+    <Box width="400px" sx={{width : {x1 : '1488px'}}} m="auto">
+        <Navbar /> 
+         <Routes>
+            <Route path="/" element={ <Home/> } />
+            <Route path="/exercise/:id" element= {<ExerciseDetail />} />
+         </Routes>
+         < Footer />
     </Box>
   )
 }
