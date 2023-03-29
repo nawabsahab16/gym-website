20ab0b2eafe81bac39c34ebf95249615a9781c5f
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-29 16:43:04 +0530

    "Initial commit: set up basic structure HOME

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {Box} from '@mui/material';
+
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
+
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
   )
 }
 
