59d70244d1a859aebec36660832c08bcdb9cd370
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-30 19:37:19 +0530

    fetching data

---
+++ src/utils/fetchData.js
@@ -0,0 +1,18 @@
+export const exerciseOptions = {
+    method: 'GET',
+  
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+
+
+
+
+export const fetchData = async (url, options) => {
+    const response = await fetch(url, options );
+    const data = await response.json();
+
+    return data;
+}
