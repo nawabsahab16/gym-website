947f5439494ec5bf3048626518a3f30fc1bd1cc1
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-30 16:49:17 +0530

    add buttons and background of exercises

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React,  { useEffect, useState} from 
+'react';
+
+import {Box, Button, Stack , TextField, Typography} from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems= "center" mt="37px"
+    justifyContent="center" p="20px">
+       <Typography fontWeight={700} sx={{
+        fontSize : {lg : '44px', xs: '30px'}}}
+        mb="50px" textAlign="center">
+        Awesome Exercise You <br/>
+        Should Know 
+       </Typography>
+
+    </Stack>
   )
 }
 
