9dfde933de310763b59fb08a0692da9d688b6df2
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-30 16:48:56 +0530

    added styles for web

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -17,17 +17,32 @@ const HeroBanner = () => {
      </Typography>
      <Typography fontWeight={700}
      sx={{ fontSize : {lg : '44px', xs: '40px'}
-    }}>
+    }}
+      mb="23px" mt="30px">
+    
         Sweat, Smile <br/> and Repeat
      </Typography>
      <Typography fontSize="22px"
-     lineHeight="35px">
+     lineHeight="35px" mb ={4} >
         Check out the most effective exercises 
      </Typography>
      <Button variant="contained"
-     color="error" href="#exercises"> 
-        Explore Exercises
-     </Button>
+     color="error" href="#exercises"
+     sx={{ backgroundColor: '#ff2625',
+       padding: '10px'}}
+     > Explore Exercises
+     </Button> 
+     <Typography 
+       fontWeight={600}
+       color= "#ff2625"
+       sx={{
+         opacity : 0.1,
+         display: {lg: 'block', xs: 'none'} 
+        }} 
+        fontSize = "200px"
+        >
+        Exercise
+     </Typography>
      <img src={HeroBannerImage} alt="banner"
      className="hero-banner-img"/>
 
