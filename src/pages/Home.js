6e397e7d5ae2e4b65beba2f71db2b248f909f23c
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-31 23:57:39 +0530

    final commit for js file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,21 @@
-import React, {useState} from 'react';
-import {Box} from '@mui/material';
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
 
 import Exercises from '../components/Exercises';
 import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
 
-
-
 const Home = () => {
-
-  const [bodyPart, setBodyPart] = useState('all')
   const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises 
-      
-      setExercises={setExercises}
-      bodyPart = {bodyPart} 
-      setBodyPart={setBodyPart} 
-       />
-      <Exercises 
-      
-      setExercises={setExercises}
-      bodyPart = {bodyPart} 
-      setBodyPart={setBodyPart} 
-      />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
