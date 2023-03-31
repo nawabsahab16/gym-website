1fd53bcdcb7e19bf72eae49f2829b3c39d31ca1f
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-31 23:57:36 +0530

    final commit for js file

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,18 +1,22 @@
 export const exerciseOptions = {
-    method: 'GET',
-  
-    headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-    }
-  };
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
 
-
-
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'aedff815f2mshff4e5da66ca3a11p1d4ce8jsna3fd75571a23',
+  },
+};
 
 export const fetchData = async (url, options) => {
-    const response = await fetch(url, options );
-    const data = await response.json();
+  const res = await fetch(url, options);
+  const data = await res.json();
 
-    return data;
-}
+  return data;
+};
