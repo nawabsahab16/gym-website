4c53f2f94a53d22bdeab94547dd0137c7bd31e45
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-31 16:25:38 +0530

    ADDED HORIZONTAL section for exercises

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,8 +1,31 @@
 import React from 'react'
 
-const HorizontalScrollbar = () => {
+import { Box } from '@mui/material';
+
+const HorizontalScrollbar = ({ data }) => {
   return (
-    <div>HorizontalScrollbar</div>
+    <div>
+      {data.map((item) => (
+        <Box 
+        key ={item.id || item}
+        itemId= {item.id || item }
+        title = {item.id || item} 
+        m="0 40px"
+
+
+        >
+
+          {item}
+
+
+
+        </Box>
+      )
+      
+      )}
+
+
+    </div>
   )
 }
 
