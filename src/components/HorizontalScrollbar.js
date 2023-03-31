9194fa91abd1424525bbeb90a3e968b0e05ad03b
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-31 19:14:27 +0530

    updated

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React , { useContext } from 'react'
 
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 
-import BodyParts from './BodyParts';
 
-const HorizontalScrollbar = ({ data }) => {
+import BodyPart from './BodyPart';
+
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
   return (
-    <div>
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+
+
+const HorizontalScrollbar = ({ data , bodyPart, setBodyPart }) => {
+  return (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow= {RightArrow}>
       {data.map((item) => (
         <Box 
         key ={item.id || item}
@@ -24,9 +51,9 @@ const HorizontalScrollbar = ({ data }) => {
       
       )}
 
-
-    </div>
+    </ScrollMenu>
+    
   )
 }
 
-export default HorizontalScrollbar
+export default HorizontalScrollbar;
